f3c13d6909ece7a3ed8e285217b57b6d035190e9
striverz <striverzarc@gmail.com>
2025-03-03 12:33:36 +0530

    added the assignment questions from the Async JS

--- 01_AsyncJS/AsyncJS/async6.js
+++ 01_AsyncJS/AsyncJS/async6.js
@@ -19,3 +19,29 @@ async function solve() {
   console.log("Hai Hello There");
 }
 solve();
+
+//2nd Assignment Question
+
+const fs = require("fs");
+function cleanFile(filePath, cb) {
+  return new Promise(function (resolve) {
+    fs.readFile(filePath, "utf-8", function (err, data) {
+      data = data.trim();
+      fs.writeFile(filePath, data, function () {
+        resolve();
+      });
+    });
+  });
+}
+
+// Q: Write a function that
+// Reads the contents of a file
+// Trims the extra space from the left and right
+// Writes it back to the file
+
+async function main() {
+  await cleanFile("m.txt");
+  console.log("Done cleaning file");
+}
+
+main();
