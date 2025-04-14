4d09ab76f0073211fbecff4ee1b80b529a6de897
striverz <striverzarc@gmail.com>
2025-04-14 16:20:26 +0530

    learning zustand react state management

---
+++ _Zustand/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Todo from "./Todo";
+
+function App() {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ _Zustand/src/todoStore.js
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+const todoStore = (set, get) => ({
+  //state
+  todos: [],
+
+  //actions or methods
+});
+const useTodoStore = create(todoStore);
