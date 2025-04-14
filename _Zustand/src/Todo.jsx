03f5c096c3c4b22bce4ddcfc44cf7d8f24780021
striverz <striverzarc@gmail.com>
2025-04-14 16:41:05 +0530

    learned about the zustand(react state management library)

--- _Zustand/src/Todo.jsx
+++ _Zustand/src/Todo.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import useTodoStore from "./todoStore";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  const { addTodo, deleteTodo, filterEvenTodos } = useTodoStore();
+  const todoData = useTodoStore((state) => state.todos);
+
+  const [todoTask, setTodoTask] = useState("");
+  const [id, setId] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input
+          value={todoTask}
+          onChange={(e) => setTodoTask(e.target.value)}
+        ></input>
+        <button onClick={() => addTodo(todoTask)}>Add Todo</button>
+        <input
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        ></input>
+        <button onClick={() => deleteTodo(id)}>Delete Todo</button>
+      </div>
+      <div>
+        <button onClick={filterEvenTodos}>Even Todos</button>
+      </div>
+
+      {/* Displaying the todoDetails */}
+
+      <div>
+        {todoData &&
+          todoData.map((todo) => (
+            <div key={todo?.id}>
+              <h1>
+                {todo.id}
+                {todo?.task}
+              </h1>
+              <span>{todo.status}</span>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
--- _Zustand/src/todoStore.js
+++ _Zustand/src/todoStore.js
@@ -1,9 +1,31 @@
 import { create } from "zustand";
-
+let currentId = 1;
 const todoStore = (set, get) => ({
   //state
   todos: [],
 
   //actions or methods
+  addTodo: function (todo) {
+    const newTodo = {
+      id: currentId++,
+      task: todo,
+      status: "Not Completed",
+    };
+
+    //updating the todos with newly added task(set)
+    set({ todos: [...get().todos, newTodo] });
+  },
+
+  deleteTodo: function (id) {
+    id = parseInt(id);
+    const deleteAndUpdatedTodo = get().todos.filter((todo) => todo.id != id);
+    set({ todos: deleteAndUpdatedTodo });
+  },
+
+  filterEvenTodos: () => {
+    const evenTodos = get().todos.filter((todo) => todo.id % 2 == 0);
+    set({ todos: evenTodos });
+  },
 });
 const useTodoStore = create(todoStore);
+export default useTodoStore;
